97f3acec3bf567ff45dbcb6c45392f4c4ffcc462
unknown <enaardayfio@gmail.com>
2018-01-26 21:06:25 +0000

    first commit

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div id="primary" class="content-area container">
+		<main id="main" class="site-main row">
 
 		<?php
 		if ( have_posts() ) :
@@ -32,13 +32,9 @@ get_header(); ?>
 			while ( have_posts() ) : the_post();
 			?>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				 <div class='col-md-4'>
-				<?php the_content()//get_template_part( 'template-parts/content', get_post_format() ); ?>
+		
+				 <div class='col-md-3'>
+				<?php the_excerpt()//get_template_part( 'template-parts/content', get_post_format() ); ?>
 				</div>
 			<?php
 			endwhile;
